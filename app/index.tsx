92a782266fab5e45132180d5f36389fd4c3f53c2
Sumit Nalavade <vs.nalavade2003@gmail.com>
2023-10-22 03:34:52 -0500

    Remove some dependencies

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,8 @@
-import { Text } from 'react-native';
-
+import { SafeAreaView, Text } from 'react-native';
 const Home = () => (
-    <Text>Hello World</Text>
+    <SafeAreaView className='flex flex-1 justify-center items-center'>
+        <Text>Hello World</Text>
+    </SafeAreaView>
 );
 
 export default Home;
--- src/templates/Welcome.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-const Welcome = () => (
-  <View className="flex-1 items-center justify-center">
-    <Text>Open up App/ to start working on your app!</Text>
-  </View>
-);
-
-export { Welcome };
