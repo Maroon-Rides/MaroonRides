213c68be41a323339617fb41481ec434a70cae19
bwees <brandonwees@gmail.com>
2023-10-23 15:33:42 -0500

    add recenter button and animate map to position

--- app/components/MapView.tsx
+++ app/components/MapView.tsx
@@ -1,27 +1,42 @@
 import React, { useState, useEffect } from "react";
-import MapView from 'react-native-maps';
+import MapView, { AnimatedRegion } from 'react-native-maps';
 import * as Location from 'expo-location';
-
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { styled } from 'nativewind';
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const StyledMapView = styled(MapView);
 
 const Index: React.FC = () => {
-    const [location, setLocation] = useState<any>();
+    var mapViewRef: any;
+
+    async function recenterView() {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+            return;
+        }
+
+        let location = await Location.getCurrentPositionAsync({
+            accuracy: Location.Accuracy.Balanced,
+            timeInterval: 2
+        });
+
+        if (!mapViewRef) {
+            return;
+        }
+
+        mapViewRef.animateToRegion({
+            latitude: location.coords.latitude, 
+            longitude: location.coords.longitude, 
+            latitudeDelta: 0.019075511625736397, 
+            longitudeDelta: 0.011273115836317515 
+        }, 250);
+    }
+
 
     useEffect(() => {
         (async () => {
-            let { status } = await Location.requestForegroundPermissionsAsync();
-            if (status !== 'granted') {
-                return;
-            }
-
-            let location = await Location.getCurrentPositionAsync({
-                accuracy: Location.Accuracy.Balanced,
-                timeInterval: 2
-            });
-
-            setLocation({ latitude: location.coords.latitude, longitude: location.coords.longitude, latitudeDelta: 0.019075511625736397, longitudeDelta: 0.011273115836317515 });
+            recenterView()
         })();
     }, []);
     
@@ -29,9 +44,33 @@ const Index: React.FC = () => {
     return (
         <StyledMapView 
             showsUserLocation={true}
-            region={location}
-            className='w-full h-full' />
+            className='w-full h-full'
+            ref = {(mapView) => { mapViewRef = mapView; }}
+        >
+                <TouchableOpacity style={styles.recenter}>
+                    <Ionicons 
+                        name="navigate" 
+                        size={24} 
+                        color="gray" 
+                        onPress={() => recenterView()} />
+                </TouchableOpacity>
+        </StyledMapView>
     )
 }
+
+const styles =  StyleSheet.create({
+    recenter: {
+        position: "absolute", 
+        top: 16, 
+        right: 16,
+        width: 50,
+        height: 50,
+        backgroundColor: "white",
+        borderRadius: 8,
+        overflow: "hidden",
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
 
 export default Index;
