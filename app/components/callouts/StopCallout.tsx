08fa35fdf5ddce43a5cbc3466f43407fe6bd2581
bwees <brandonwees@gmail.com>
2023-11-03 22:25:39 -0500

    remove icon

--- app/components/callouts/StopCallout.tsx
+++ app/components/callouts/StopCallout.tsx
@@ -6,7 +6,6 @@ export default function StopCallout({stop, tintColor, routeName}) {
     return (
         <Callout>
             <View className="w-20">
-                <BusIcon sizing = "w-12 h-10" busRoute = {stop} textSize = {18} name = {stop.shortName} color = {stop.routeInfo.color}/>
                 <Text className="font-bold text-m">{stop.name}</Text>
                 <Text>{stop.description}</Text>
             </View>
