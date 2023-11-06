69fe1afa4015a7dad2c7c3c927e6df418cf651d3
Eli410 <mcqueen0410@hotmail.com>
2023-11-06 15:49:34 -0600

    added auto-scroll that works half of the time

--- app/components/Timetable.tsx
+++ app/components/Timetable.tsx
@@ -12,119 +12,151 @@ function Timetable({ timetable, flatListHeight, highlightColor }) {
     const [selectedIndex, setSelectedIndex] = useState(0)
     const flatListRef = useRef(null);
 
-    const [indexToScrollTo, setIndexToScrollTo] = useState(3); //to be implimented
-    
-    
-    function formatTime(date) {
-        const hours = date.getHours() % 12 || 12; 
-        const minutes = date.getMinutes();
-        const amPm = date.getHours() < 12 ? 'AM' : 'PM';
-    
-        const paddedHours = String(hours).padStart(2, '0');
-        const paddedMinutes = String(minutes).padStart(2, '0');
-    
-        return `${paddedHours}:${paddedMinutes} ${amPm}`;
-    }
+    const [processedTimetable, setProcessedTimetable] = useState<string[][]>(processTable(0)); //to be implimented
+    const [indexToScrollTo, setIndexToScrollTo] = useState(-1); //to be implimented
+
     
 
 
     const currentTime = new Date();
 
-    //attempt to find the row index to highlight
-    // Object.keys(timetable[selectedIndex]).forEach((currentData, outerIndex) => {
-    //     timetable[selectedIndex][currentData].forEach((innerData, innerIndex) => {
-    //         if (innerData < currentTime) {
-    //             if(indexToScrollTo==-1){
-    //                 setIndexToScrollTo(innerData)
-    //             }
-    //         }
-    //     });
-    // });
 
+    function findHighlight(index): int {
+        var stops = Object.keys(timetable[index]);
+        var numTimes = timetable[index][stops[0]].length;
+        let result = 0;  // Default value
     
+        outerLoop:  // Label for the outer loop
+        for (var i = 0; i < numTimes; i++) {
+            for (var j = 0; j < stops.length; j++) {
+                if (timetable[index][stops[j]][i] > currentTime && indexToScrollTo === -1) {
+                    result = i;
+                    // console.log(result);
+                    break outerLoop;  // Break out of the outer loop
+                }
+            }
+        }
+    
+        return result;
+    }
+    
+
+    function processTable(index): string[][] {
+        var table = [];
+
+        
+        var numTimes = timetable[index][Object.keys(timetable[index])[0]].length;
+        
+        for (var i = 0; i < numTimes; i++) {
+            var row: string[] = [];
+            Object.keys(timetable[index]).forEach((stop) => {
+                row.push(timetable[index][stop][i])
+            })
+            table.push(row)
+        }
+
+        return table;
+    }
+
+
+
+    useEffect(() => {
+        setProcessedTimetable(processTable(selectedIndex))
+        setIndexToScrollTo(findHighlight(selectedIndex))
+    }, [selectedIndex])
+    //convert the table to a 2d array
+
+
+    // useEffect(() => {
+    //     if (flatListRef.current) {
+            
+    //         flatListRef.current.scrollToIndex({ animated: true, index: indexToScrollTo });
+    //     }
+    // }, [selectedIndex]); 
+
+
+
     return (
         <View>
             <SegmentedControl
-            values={[stops[stops.length-1], stops[0]]}
-            selectedIndex={selectedIndex}
-            onChange={(event) => setSelectedIndex(event.nativeEvent.selectedSegmentIndex)}
+                values={[stops[stops.length-1], stops[0]]}
+                selectedIndex={selectedIndex}
+                onChange={(event) => {
+                    setSelectedIndex(event.nativeEvent.selectedSegmentIndex);
+                    setIndexToScrollTo(-1);
+                    }}
             />
 
-            <View style={{ flexDirection: "row", paddingTop: 20}}>
-                
-            </View>           
+                  
             <View style={{height: 1, 
                         width: '100%', 
                         backgroundColor: '#F2F2F2',
-                        marginTop: 15}} />
+                        marginTop: 8}} 
+            />
 
+            <FlatList 
+                ref={flatListRef}
+                key={`flatlist-${selectedIndex}`}
+                style={{ height: flatListHeight-150}}
+                data={processedTimetable}
+                stickyHeaderIndices={[0]}
+                ListHeaderComponent={
+                    <View className='flex-row justify-between'>
+                        {
+                            Object.keys(timetable[selectedIndex]).map((stop, index) => (
+                                <Text 
 
+                                    className="font-bold flex-1 text-center flex-shrink-1 bg-white pb-2 pt-2"
+                                    numberOfLines={2} 
+                                    ellipsizeMode='tail' 
+                                    key={index}
+                                >
+                                    {stop}
+                                </Text>
+                            ))
+                        }
+                    </View>
+                }
+        
+                initialNumToRender={indexToScrollTo + 1}
 
-<FlatList 
-    ref={flatListRef}
-    key={`flatlist-${selectedIndex}`}
-    style={{ height: flatListHeight-150}}
-    data={Object.keys(timetable[selectedIndex])}
-    numColumns={Object.keys(timetable[selectedIndex]).length}
-    stickyHeaderIndices={[0]}
-    ListHeaderComponent={
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-            {
-                Object.keys(timetable[selectedIndex]).map((stop, index) => (
-                    <Text 
-                        style={{
-                            fontWeight: 'bold',
-                            flex: 1,
-                            textAlign: 'center', // to center the text in its container
-                            flexShrink: 1,
-                            backgroundColor:'white'
-                        }} 
-                        numberOfLines={2} 
-                        ellipsizeMode='tail' 
-                        key={index}
-                    >
-                        {stop}
-                    </Text>
-                ))
-            }
+                renderItem={({ item: row, index }) => {
+                    return (
+                        <View className='flex-grow flex-row rounded-lg' style={{ backgroundColor: index === indexToScrollTo ? highlightColor+'33' : (index % 2 == 0 ? '#F2F2F2' : 'white')}}>
+                            {row.map((col, innerIndex) => (
+                                <Text style={{ 
+                                        color: index==indexToScrollTo ? highlightColor : (col > currentTime ? 'black' : '#707373'),
+                                        paddingTop: 10,
+                                        paddingBottom: 10,
+                                        textAlign: 'center',
+                                        flex:1,
+                                        opacity: col===undefined? 0 : 1
+                                    }}
+                                    key={innerIndex}
+                                >
+                                    {col!=undefined? col.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) : 'NO DATA'}
+                                </Text>
+                            ))}
+                        </View>
+                    );
+                    
+                }}
+
+                onLayout={() => {
+                    flatListRef.current.scrollToIndex({animation: false, index: indexToScrollTo });
+                }} 
+
+                onScrollToIndexFailed={() => {
+                    setTimeout(() => {
+                        flatListRef.current.scrollToIndex({animation: false, index: indexToScrollTo})
+                        
+                        }, 0); //react native FTW best workaround ever
+                        //remove the timeout and it doesn't work
+                    }}  
+                
+                />
+
         </View>
-    }
-    //callback when flatlist is loaded
-    // onLayout={flatListRef.current.scrollToIndex(indexToScrollTo)}
-    renderItem={({ item: data, index }) => {
-
-
-        return (
-            <View style={{
-                flexDirection: 'column',
-                flex:1
-            }}>
-                {timetable[selectedIndex][data].map((data, innerIndex) => (
-                    <Text style={{ 
-                            color: data > currentTime ? highlightColor : '#707373',
-                            paddingTop: 10,
-                            paddingBottom: 10,
-                            backgroundColor: innerIndex === indexToScrollTo ? highlightColor+'33' : (innerIndex % 2 == 0 ? '#F2F2F2' : 'white'), 
-                            textAlign: 'center',
-                            flex:1
-                        }}
-                        key={innerIndex}
-                    >
-                        {formatTime(data)}
-                    </Text>
-                ))}
-            </View>
-        );
-         
-    }}
-    
-/>
-
-
-    </View>
-    )
-    
-                            
-}
+)}
 
 export default Timetable;
