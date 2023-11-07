e78ca08f1290ccdd6a14db3a27556463e8df9f85
Sumit Nalavade <vs.nalavade2003@gmail.com>
2023-11-07 14:30:33 -0600

    Fix type errors, merge conflicts and scroll bugs

--- app/components/Timetable.tsx
+++ app/components/Timetable.tsx
@@ -1,17 +1,151 @@
-import { ActivityIndicator, Text, View } from 'react-native'
-import React, { useEffect } from 'react'
+import { Text, View, ScrollView } from "react-native";
+import React, { useState, useEffect } from "react";
+import SegmentedControl from "@react-native-segmented-control/segmented-control";
+import { ITimetable } from "utils/interfaces";
 
-function Timetable({ timetable, tintColor }) {
-    // Uncomment to view the incoming data!
+interface Props {
+    timetable: ITimetable;
+    highlightColor: string;
+}
+
+const Timetable: React.FC<Props> = ({
+    timetable,
+    highlightColor,
+}) => {
+    const stops = Object.keys(timetable[0]!);
+    const [selectedIndex, setSelectedIndex] = useState(0);
+
+    const [processedTimetable, setProcessedTimetable] = useState<string[][]>(
+        processTable(0)
+    ); //to be implimented
+    const [indexToScrollTo, setIndexToScrollTo] = useState(-1); //to be implimented
+
+    const currentTime = new Date();
+
+    function findHighlight(index: number) {
+        var stops = Object.keys(timetable[index]!);
+        var numTimes = timetable[index][stops[0]].length;
+        let result = 0; // Default value
+
+        // Label for the outer loop
+        outerLoop: for (var i = 0; i < numTimes; i++) {
+            for (var j = 0; j < stops.length; j++) {
+                if (
+                    timetable[index][stops[j]][i] > currentTime &&
+                    indexToScrollTo === -1
+                ) {
+                    result = i;
+                    break outerLoop; // Break out of the outer loop
+                }
+            }
+        }
+
+        return result;
+    }
+
+    function processTable(index: number) {
+        var table = [];
+
+        var numTimes = timetable[index][Object.keys(timetable[index])[0]].length;
+
+        for (var i = 0; i < numTimes; i++) {
+            var row: string[] = [];
+            Object.keys(timetable[index]).forEach((stop) => {
+                row.push(timetable[index][stop][i]);
+            });
+            table.push(row);
+        }
+
+        return table;
+    }
+
     useEffect(() => {
-    }, [timetable])
+        setProcessedTimetable(processTable(selectedIndex));
+        setIndexToScrollTo(findHighlight(selectedIndex));
+    }, [selectedIndex]);
 
     return (
         <View>
-            <Text>{Object.keys(timetable[0])}</Text>
+            <SegmentedControl
+                values={[stops[stops.length - 1]!, stops[0]!]}
+                selectedIndex={selectedIndex}
+                onChange={(event) => {
+                    setSelectedIndex(event.nativeEvent.selectedSegmentIndex);
+                    setIndexToScrollTo(-1);
+                }}
+            />
+
+            <View
+                style={{
+                    height: 1,
+                    width: "100%",
+                    backgroundColor: "#F2F2F2",
+                    marginTop: 8,
+                }}
+            />
+
+
+            <ScrollView>
+                <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+                    {Object.keys(timetable[selectedIndex]!).map((stop, index) => (
+                        <Text
+                            key={index}
+                            style={{
+                                flex: 1,
+                                textAlign: "center",
+                                backgroundColor: "white",
+                                paddingVertical: 2,
+                            }}
+                        >
+                            {stop}
+                        </Text>
+                    ))}
+                </View>
+
+                {processedTimetable.map((row, index) => (
+                    <View
+                        key={index}
+                        style={{
+                            flexDirection: "row",
+                            backgroundColor:
+                                index === indexToScrollTo
+                                    ? `${highlightColor}33`
+                                    : index % 2 === 0
+                                        ? "#F2F2F2"
+                                        : "white",
+                            borderRadius: 8,
+                        }}
+                    >
+                        {row.map((col, innerIndex) => (
+                            <Text
+                                key={innerIndex}
+                                style={{
+                                    color:
+                                        index === indexToScrollTo
+                                            ? highlightColor
+                                            : new Date(col) > currentTime
+                                                ? "black"
+                                                : "#707373",
+                                    paddingTop: 10,
+                                    paddingBottom: 10,
+                                    textAlign: "center",
+                                    flex: 1,
+                                    opacity: col === undefined ? 0 : 1,
+                                }}
+                            >
+                                {col !== undefined
+                                    ? new Date(col).toLocaleTimeString([], {
+                                        hour: "2-digit",
+                                        minute: "2-digit",
+                                    })
+                                    : "NO DATA"}
+                            </Text>
+                        ))}
+                    </View>
+                ))}
+            </ScrollView>
         </View>
-        
-    )
-}
+    );
+};
 
 export default Timetable;
--- utils/interfaces.ts
+++ utils/interfaces.ts
@@ -1,76 +1,76 @@
 export interface IBusRoute {
-    id: string,
-    name: string,
-    color: string,
-    onCampus: boolean,
-    shortName: string // text to show in colored icons
+    id: string;
+    name: string;
+    color: string;
+    onCampus: boolean;
+    shortName: string; // Text to show in colored icons
+    key: string;
+    description?: string;
+    category: "On Campus" | "Off Campus" | "Gameday";
+    endpointName: string;
+    routeInfo: {
+        routeKey: string;
+        color: string;
+        patternPaths: {
+            patternKey: string;
+            patternPoints: IPatternPoint[];
+        }[];
+    };
+    routePatterns: IRoutePattern[];
 }
 
-export interface IBusRoute {
-    key: string
-    name: string
-    shortName: string
-    description?: string
-    category: "On Campus" | "Off Campus" | "Gameday"
-    endpointName: string
-    routeInfo: {
-        routeKey: string
-        color: string
-        patternPaths: {
-            patternKey: string
-            patternPoints: [
-                {
-                    key: string
-                    name: string
-                    description?: string
-                    rank: number
-                    longitude: number
-                    latitude: number
-                    isStop: boolean
-                    isTimePoint: boolean
-                    routeHeaderRank: number
-                    distanceToPreviousPoint: number
-                    stop: {
-                        key: string
-                        name: string
-                        stopCode: number
-                        isTemporary: boolean
-                        attributes: any[]
-                    }
-                }
-            ]
-        }[]
-    }
-    routePatterns: [
-        {
-            key: string
-            name: string
-            shortName: number
-            description?: string
-            direction: {
-                key: string
-                name: string
-            }
-            destination: string
-            lineDisplayInfo: {
-                color: string
-                type: number
-                symbol: number
-                size: number
-            }
-            timePointDisplayInfo: {
-                color: string
-                type: number
-                symbol: number
-                size: number
-            }
-            busStopDisplayInfo: {
-                color: string
-                type: number
-                symbol: number
-                size: number
-                isDisplay: boolean
-            }
-        }
-    ]
+export interface IPatternPoint {
+    key: string;
+    name: string;
+    description?: string;
+    rank: number;
+    longitude: number;
+    latitude: number;
+    isStop: boolean;
+    isTimePoint: boolean;
+    routeHeaderRank: number;
+    distanceToPreviousPoint: number;
+    stop: {
+        key: string;
+        name: string;
+        stopCode: number;
+        isTemporary: boolean;
+        attributes: any[];
+    };
 }
+
+export interface IRoutePattern {
+    key: string;
+    name: string;
+    shortName: number;
+    description?: string;
+    direction: {
+        key: string;
+        name: string;
+    };
+    destination: string;
+    lineDisplayInfo: IDisplayInfo;
+    timePointDisplayInfo: IDisplayInfo;
+    busStopDisplayInfo: IBusStopDisplayInfo;
+}
+
+export interface IDisplayInfo {
+    color: string;
+    type: number;
+    symbol: number;
+    size: number;
+}
+
+export interface IBusStopDisplayInfo {
+    color: string
+    type: number;
+    symbol: number;
+    size: number;
+    isDisplay: boolean;
+}
+
+export type ITimestamp = string;
+
+export type ILocationData = Record<string, ITimestamp[]>;
+
+export type ITimetable = ILocationData[];
