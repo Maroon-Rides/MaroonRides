5c5b2430da05756a7c7fa4e01b92d6c640098585
bwees <brandonwees@gmail.com>
2023-11-12 19:52:32 -0600

    stop callout

--- app/components/callouts/BusCallout.tsx
+++ app/components/callouts/BusCallout.tsx
@@ -1,22 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Callout } from 'react-native-maps'
+import BusIcon from '../BusIcon'
 
 export default function BusCallout({bus, tintColor, routeName}) {
   return (
     <Callout>
         <View className="w-20">
             <Text>Name: {bus.name}</Text>
-            <View className={`w-10 h-7 rounded-lg mr-3 content-center justify-center`} style={{ backgroundColor: "#" + tintColor}}>
-                    <Text
-                        adjustsFontSizeToFit={true}
-                        numberOfLines={0}
-                        className="text-center font-bold text-white p-1"
-                        style={{ fontSize: 24 }} // this must be used, nativewind is broken :(
-                    >
-                        {routeName}
-                     </Text>
-                </View>
+            <BusIcon name={routeName} color={tintColor} sizing="w-10 h-6" textSize={18}/>
         </View>
     </Callout>
   )
