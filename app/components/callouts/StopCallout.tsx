639cba6c1a18d18068f0b9f71da8b691944b0aef
Sumit Nalavade <vs.nalavade2003@gmail.com>
2024-01-05 02:06:01 -0600

    Fix callouts on Android

--- app/components/callouts/StopCallout.tsx
+++ app/components/callouts/StopCallout.tsx
@@ -1,26 +1,26 @@
-import React, { useEffect, useState } from 'react'
-import { View, Text } from 'react-native'
+import React, { useEffect, useState, memo } from 'react'
+import { View, Text, Platform } from 'react-native'
 import { Callout } from 'react-native-maps'
 
-import BusIcon from '../ui/BusIcon'
 import useAppStore from '../../stores/useAppStore'
-import { IGetNextDepartTimesResponse, IStop } from '../../../utils/interfaces'
+import { IGetNextDepartTimesResponse } from '../../../utils/interfaces'
 
 interface Props {
-    stop: IStop
-    tintColor: string
+    stopName: string
+    stopCode: string
     routeName: string
+    tintColor: string
 }
 
-const StopCallout: React.FC<Props> = ({ stop, tintColor, routeName }) => {
-    const [contentSize, setContentSizing] = useState([0, 15]);
-    
-    const [estimate, setEstimate] = useState<IGetNextDepartTimesResponse | undefined>(undefined);
+const StopCallout: React.FC<Props> = ({ stopName, stopCode, tintColor, routeName }) => {
+    const [contentSize, setContentSizing] = useState([100, 15]);
+
+    const [estimate, setEstimate] = useState<IGetNextDepartTimesResponse | null>(null);
     const stopEstimates = useAppStore((state) => state.stopEstimates);
 
     useEffect(() => {
         stopEstimates.forEach((stopEstimate) => {
-            if (stopEstimate.stopCode === stop.stopCode) {
+            if (stopEstimate.stopCode === stopCode) {
                 setEstimate(stopEstimate.departureTimes);
             }
         })
@@ -28,13 +28,17 @@ const StopCallout: React.FC<Props> = ({ stop, tintColor, routeName }) => {
 
     // TODO: show amenities
     return (
-        <Callout style={{alignItems: 'center', width: contentSize[0], height: contentSize[1]}} >
-            <View  style={{ flexDirection: "row", justifyContent: "center", alignItems: "center", alignSelf: "flex-start"}}  onLayout={(event) => { setContentSizing([event.nativeEvent.layout.width, event.nativeEvent.layout.height]) }} >
-                <BusIcon name={routeName} color={tintColor} isCallout={true} style={{ marginRight: 8 }} />
-                <Text style={{ maxWidth: 200, fontWeight: 'bold' }} numberOfLines={1}>{stop.name} {estimate?.amenities.length}</Text>
+        <Callout style={{ alignItems: 'center', width: contentSize[0], height: contentSize[1], minWidth: 95, minHeight: Platform.OS === 'android' ? 40 : 1 }} >
+            <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center", alignSelf: "flex-start", paddingTop: Platform.OS === 'android' ? 8 : 0 }} onLayout={(event) => { setContentSizing([event.nativeEvent.layout.width, event.nativeEvent.layout.height]) }} >
+                <View style={{ backgroundColor: tintColor, borderRadius: 6, alignItems: 'center', justifyContent: "center", width: 40, height: 24, marginRight: 8 }}>
+                    <Text adjustsFontSizeToFit={true} style={{ fontSize: 18, textAlign: 'center', fontWeight: 'bold', color: 'white', padding: 4 }}>
+                        {routeName}
+                    </Text>
+                </View>
+                <Text style={{ maxWidth: 200, fontWeight: 'bold' }} numberOfLines={1}>{stopName} {estimate?.amenities.length}</Text>
             </View>
         </Callout>
     )
 }
 
-export default StopCallout;
+export default memo(StopCallout);
