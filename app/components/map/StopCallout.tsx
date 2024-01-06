34924687cbc16f90dd903cf744ebb63fb51c4c2a
bwees <brandonwees@gmail.com>
2024-01-05 21:25:24 -0500

    amenity row changes

--- app/components/map/StopCallout.tsx
+++ app/components/map/StopCallout.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { View, Text } from 'react-native'
 import { Callout } from 'react-native-maps'
-
 import BusIcon from '../ui/BusIcon'
 import useAppStore from '../../stores/useAppStore'
 import { IGetNextDepartTimesResponse, IStop } from '../../../utils/interfaces'
+import AmenityRow from '../ui/AmenityRow'
 
 interface Props {
     stop: IStop
@@ -22,6 +22,7 @@ const StopCallout: React.FC<Props> = ({ stop, tintColor, routeName }) => {
         stopEstimates.forEach((stopEstimate) => {
             if (stopEstimate.stopCode === stop.stopCode) {
                 setEstimate(stopEstimate.departureTimes);
+                console.log(stopEstimate.departureTimes)
             }
         })
     }, [stopEstimates])
@@ -29,9 +30,13 @@ const StopCallout: React.FC<Props> = ({ stop, tintColor, routeName }) => {
     // TODO: show amenities
     return (
         <Callout style={{alignItems: 'center', width: contentSize[0], height: contentSize[1]}}>
-            <View  style={{ flexDirection: "row", justifyContent: "center", alignItems: "center", alignSelf: "flex-start"}}  onLayout={(event) => { setContentSizing([event.nativeEvent.layout.width, event.nativeEvent.layout.height]) }} >
-                <BusIcon name={routeName} color={tintColor} isCallout={true} style={{ marginRight: 8 }} />
-                <Text style={{ maxWidth: 200, fontWeight: 'bold' }} numberOfLines={1}>{stop.name} {estimate?.amenities.length}</Text>
+            <View  onLayout={(event) => { setContentSizing([event.nativeEvent.layout.width, event.nativeEvent.layout.height]) }} >
+                <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center", alignSelf: "flex-start"}}  >
+                    <BusIcon name={routeName} color={tintColor} isCallout={true} style={{ marginRight: 8 }} />
+                    <Text style={{ maxWidth: 200, fontWeight: 'bold' }} numberOfLines={1}>{stop.name}</Text>
+                </View>
+
+                <AmenityRow amenities={estimate?.amenities ?? []} color='grey' size={20} style={{marginTop: 4}}/>
             </View>
         </Callout>
     )
--- app/components/ui/AmenityRow.tsx
+++ app/components/ui/AmenityRow.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { View, ViewProps } from 'react-native'
 import { IAmenity } from 'utils/interfaces'
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons, FontAwesome5, MaterialIcons } from '@expo/vector-icons';
 
 interface Props extends ViewProps {
     amenities: IAmenity[]
@@ -16,11 +16,11 @@ const AmenityRow: React.FC<Props> = ({amenities, color, size, style}) => {
           <View style={[{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }, style]}>
             {amenities.map((amenity: IAmenity) => (
                 // switch case
-                <View key={amenity.name} style={{paddingLeft: 2}}>
+                <View key={amenity.name} style={{paddingLeft: 4}}>
                     {amenity.name == "Air Conditioning" && <MaterialCommunityIcons name="air-conditioner" size={size} color={color} />}
-                    {amenity.name == "Wheelchair Accessible" && <MaterialCommunityIcons name="wheelchair-accessibility" size={size} color={color} />}
-                    {amenity.name == "Wheelchair Lift" && <MaterialCommunityIcons name="wheelchair-accessibility" size={size} color={color} />}
-                    {amenity.name == "Bicycle Rack" && <MaterialCommunityIcons name="bike" size={size} color={color} />}
+                    {amenity.name == "Wheelchair Accessible" && <FontAwesome5 name="wheelchair" size={size-4} color={color} />}
+                    {amenity.name == "Wheelchair Lift" && <FontAwesome5 name="wheelchair" size={size-4} color={color} />}
+                    {amenity.name == "Bicycle Rack" && <MaterialIcons name="pedal-bike" size={size+4} color={color} />}
                     {amenity.name == "Shelter" && <MaterialCommunityIcons name="bus-stop-covered" size={size} color={color} />}
                 </View>
                 
