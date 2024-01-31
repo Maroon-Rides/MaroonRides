398099a4f19b008ba1d6b5946013bd68220855e9
bwees <brandonwees@gmail.com>
2024-01-31 15:15:31 -0600

    initial watch app

---
+++ targets/watch/components/Bus Icon.swift
@@ -0,0 +1,18 @@
+//
+//  Bus Icon.swift
+//  Reveille Rides Watch App
+//
+//  Created by Brandon Wees on 1/31/24.
+//
+
+import SwiftUI
+
+struct Bus_Icon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Bus_Icon()
+}
---
+++ targets/watch/components/RouteList.swift
@@ -0,0 +1,18 @@
+//
+//  RouteList.swift
+//  Reveille Rides Watch App
+//
+//  Created by Brandon Wees on 1/31/24.
+//
+
+import SwiftUI
+
+struct RouteList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RouteList()
+}
