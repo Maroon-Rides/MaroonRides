338e1bbd11d0011ef22535781a2f7b0b79ea09c1
bwees <brandonwees@gmail.com>
2024-01-31 15:15:31 -0600

    initial watch app

---
+++ targets/watch/ReveilleRides_Watch.swift
@@ -0,0 +1,17 @@
+//
+//  basic_watchappApp.swift
+//  basic-watchapp
+//
+//  Created by Brandon Wees on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct basic_watchappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ targets/watch/data/AggieSpritAPI.swift
@@ -0,0 +1,8 @@
+//
+//  AggieSpritAPI.swift
+//  Reveille Rides Watch App
+//
+//  Created by Brandon Wees on 1/31/24.
+//
+
+import Foundation
