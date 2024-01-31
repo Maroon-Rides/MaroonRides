7ef3e2a8f48c2e06c795565de0302ebcc9324404
bwees <brandonwees@gmail.com>
2024-01-31 15:15:37 -0600

    more watch app stuff

--- targets/watch/ContentView.swift
+++ targets/watch/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+      NavigationStack {
+        TabView {
+          RouteList()
+            .navigationTitle("Favorites")
+          Text("HSHSHSH")
+            .navigationTitle("All Routes")
         }
-        .padding()
+      }
     }
 }
 
--- targets/watch/components/Bus Icon.swift
+++ targets/watch/components/Bus Icon.swift
@@ -8,11 +8,51 @@
 import SwiftUI
 
 struct Bus_Icon: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+  var name: String
+  var number: String
+  var color: Color
+  var subtitle: String
+
+  var body: some View {
+      HStack {
+        Text("01")
+          .padding([.vertical], 8)
+          
+          .padding([.horizontal], 12)
+          .background(color)
+          .clipShape(.rect(cornerSize: CGSize(width: 8, height: 8)))
+          
+        VStack {
+          HStack {
+            Text(name)
+              .font(.headline)
+              .frame(alignment: .leading)
+            Spacer()
+          }
+          
+          HStack {
+            Text(subtitle)
+              .font(.caption2)
+              .frame(alignment: .leading)
+              .foregroundStyle(.secondary)
+            Spacer()
+          }
+        }
+        .padding([.leading], 4)
+        
+        Spacer()
+        
+        Image(systemName: "chevron.right")
+          .foregroundStyle(.tertiary)
+      }
+  }
 }
 
 #Preview {
-    Bus_Icon()
+    Bus_Icon(
+      name: "RELLIS",
+      number: "47",
+      color: Color.red,
+      subtitle: "MSC | RELLIS"
+    )
 }
