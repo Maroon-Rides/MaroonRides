3f27d9af1413ce46abc26303881ce17e81ea8375
bwees <brandonwees@gmail.com>
2024-02-03 11:54:38 -0600

    add stylesheet to html rendering

--- app/components/sheets/AlertDetail.tsx
+++ app/components/sheets/AlertDetail.tsx
@@ -1,5 +1,5 @@
 import { BottomSheetModal, BottomSheetView} from "@gorhom/bottom-sheet";
-import { View, Text, TouchableOpacity, useWindowDimensions} from "react-native";
+import { View, TouchableOpacity, useWindowDimensions} from "react-native";
 import Ionicons from '@expo/vector-icons/Ionicons';
 import useAppStore from "../../stores/useAppStore";
 import SheetHeader from "../ui/SheetHeader";
@@ -10,6 +10,12 @@ const AlertDetails: React.FC<{ sheetRef: React.RefObject<BottomSheetModal> }> = 
   const snapPoints = ['25%', '45%', '85%'];
   const alert = useAppStore((state) => state.alertDetail);
   
+  const tagStyles = { 
+    h6: { fontSize: 20, fontWeight: "bold", marginTop: 24, marginBottom: 8}, 
+    span: {fontWeight: "bold"},
+    ul: {marginLeft: 16, marginTop: 8, padding: 8, paddingLeft: 24, backgroundColor: "#f2f2f2", borderRadius: 8},
+    div: {paddingBottom: 0, marginBottom: 0}
+  };
 
   return (
     <BottomSheetModal ref={sheetRef} snapPoints={snapPoints} index={1}>
@@ -27,7 +33,10 @@ const AlertDetails: React.FC<{ sheetRef: React.RefObject<BottomSheetModal> }> = 
             <RenderHtml
                 contentWidth={useWindowDimensions().width}
                 baseStyle={{ fontSize: 16, paddingHorizontal: 16, paddingTop: 8}}
-                source={{ html: alert?.description || '' }}
+                source={{ html: alert?.description || 'No details to show.' }}
+
+                // @ts-ignore: Werid errors with tagStyles typings, but it works
+                tagsStyles={tagStyles}
             />
         </BottomSheetView>
     </BottomSheetModal>
