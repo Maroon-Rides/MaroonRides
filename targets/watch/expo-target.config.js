f0e32039a6af7d5dc4dd498248217881a64ee332
bwees <brandonwees@gmail.com>
2024-08-02 22:38:15 -0400

    fix naming

--- targets/watch/expo-target.config.js
+++ targets/watch/expo-target.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@bacons/apple-targets').Config} */
 module.exports = {
   type: "watch",
-  name: "Maroon Rides Watch App",
+  name: "Maroon Rides",
   identifier: "com.bwees.reveille-rides.watch",
   deploymentTarget: "9.0",
   icon: "../../assets/icon.png",
