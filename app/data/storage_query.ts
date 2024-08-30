65755fc612b6ab0216102d6f8fed1f9af73eb25c
bwees <brandonwees@gmail.com>
2024-08-30 18:14:25 -0500

    fix missing query

--- app/data/storage_query.ts
+++ app/data/storage_query.ts
@@ -88,6 +88,7 @@ export const addFavoriteMutation = () => {
         },
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ["favorites"] });
+            queryClient.invalidateQueries({ queryKey: ["favorite"] });
         }
     });
 
