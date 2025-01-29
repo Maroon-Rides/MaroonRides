92fdd3ecb716fad3011e174d029684cf2d9f5870
bwees <brandonwees@gmail.com>
2025-01-29 08:12:12 -0600

    first changes, removed expo router but getting weird behavior rn

--- Main.tsx
+++ Main.tsx
@@ -1,1 +1,10 @@
-import 'expo-router/entry';
+import Home from "./app/index";
+import { registerRootComponent } from "expo";
+
+function App() {
+    return (
+        <Home />
+    );
+}
+
+registerRootComponent(App);
