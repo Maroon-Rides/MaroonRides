a39ef3372817711575170a58eff763e80a81dac5
bwees <brandonwees@gmail.com>
2025-07-27 18:12:28 -0500

    chore: babel cleanup

--- babel.config.js
+++ babel.config.js
@@ -4,15 +4,6 @@ module.exports = (api) => {
     presets: ['babel-preset-expo'],
     plugins: [
       'react-native-reanimated/plugin',
-      [
-        'module-resolver',
-        {
-          alias: {
-            // This needs to be mirrored in tsconfig.json
-            '@': './src',
-          },
-        },
-      ],
     ],
   };
 };
