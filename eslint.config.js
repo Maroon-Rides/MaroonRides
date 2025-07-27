4d618d21d54200c96939981fb721b731b7ab9f96
bwees <brandonwees@gmail.com>
2025-07-27 18:43:05 -0500

    chore: install prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require("eslint-config-expo/flat");
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 module.exports = defineConfig([
   expoConfig,
+  eslintPluginPrettierRecommended,
   {
     ignores: ["dist/*"],
     rules: {
