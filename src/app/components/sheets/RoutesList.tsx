ebe2eeb68cc46f4f80acaf467d8f1d431b787ff0
bwees <brandonwees@gmail.com>
2025-09-09 13:39:52 -0500

    chore: lint

--- src/app/components/sheets/RoutesList.tsx
+++ src/app/components/sheets/RoutesList.tsx
@@ -122,7 +122,6 @@ const RoutesList: React.FC<SheetProps> = ({ sheetRef }) => {
     appLogger.i('Refetched route groups and favorites on sheet present');
   }
 
-
   return (
     <BaseSheet
       sheetRef={sheetRef}
@@ -262,13 +261,13 @@ const RoutesList: React.FC<SheetProps> = ({ sheetRef }) => {
                   {favorites?.find(
                     (fav) => fav.routeCode === route.routeCode,
                   ) && (
-                      <FontAwesome
-                        name="star"
-                        size={16}
-                        color={theme.starColor}
-                        style={{ marginLeft: 4 }}
-                      />
-                    )}
+                    <FontAwesome
+                      name="star"
+                      size={16}
+                      color={theme.starColor}
+                      style={{ marginLeft: 4 }}
+                    />
+                  )}
                 </View>
                 {route.directions.length === 2 ? (
                   <View
