aabf3b8d02628f1499c0560159fa25c625dbd419
bwees <brandonwees@gmail.com>
2025-09-14 00:39:57 -0500

    Revert "fix: crashing when returning from route detail"
    This reverts commit e5fc08b2c8cf1dc9d79bcc8241ae774d72589c17.

--- src/app/components/map/markers/BusMarker.tsx
+++ src/app/components/map/markers/BusMarker.tsx
@@ -1,10 +1,9 @@
 import useAppStore from '@data/state/app_state';
 import { Bus, Route } from '@data/types';
 import React, { memo } from 'react';
-import { Platform, Text, View } from 'react-native';
-import { Callout, Marker } from 'react-native-maps';
-import AmenityRow from '../../ui/AmenityRow';
-import BusIcon from '../../ui/BusIcon';
+import { Platform } from 'react-native';
+import { Marker } from 'react-native-maps';
+import BusCallout from '../BusCallout';
 import BusMapIcon from '../mapIcons/BusMapIcon';
 
 interface Props {
@@ -56,99 +55,7 @@ const BusMarker: React.FC<Props> = ({ bus, route }) => {
         selectedDirection={selectedDirection}
       />
 
-      <Callout style={{ zIndex: 1000, elevation: 1000 }}>
-        <View
-          style={[
-            { width: 170 },
-            Platform.OS === 'android' && {
-              padding: 4,
-              paddingVertical: 8,
-            },
-          ]}
-        >
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'center',
-              alignItems: 'center',
-              paddingBottom: 4,
-            }}
-          >
-            <View
-              style={{
-                flexDirection: 'row',
-                justifyContent: 'center',
-                alignItems: 'center',
-                backgroundColor: '#efefef',
-                marginRight: 8,
-                borderRadius: 4,
-              }}
-            >
-              <BusIcon
-                name={route.routeCode}
-                color={route.tintColor}
-                isCallout={true}
-              />
-              <Text
-                style={{
-                  fontSize: 12,
-                  marginLeft: 6,
-                  marginRight: 4,
-                  color: 'grey',
-                }}
-              >
-                {bus.name}
-              </Text>
-            </View>
-            <View style={{ flex: 1 }} />
-            <AmenityRow amenities={bus.amenities} color="gray" size={20} />
-          </View>
-
-          {!!bus.direction && !bus.direction.isOnlyDirection && (
-            <Text
-              style={{
-                flexDirection: 'row',
-                justifyContent: 'center',
-                alignSelf: 'flex-start',
-                marginBottom: 4,
-                marginTop: 2,
-              }}
-            >
-              <Text style={{ fontWeight: '700', color: route.tintColor }}>
-                To:{' '}
-              </Text>
-              <Text style={{ fontSize: 14, marginLeft: 2, color: 'black' }}>
-                {bus.direction?.name}
-              </Text>
-            </Text>
-          )}
-          <View
-            style={{ flexDirection: 'row', justifyContent: 'space-between' }}
-          >
-            <Text
-              style={{
-                fontWeight: 'bold',
-                color: '#6B7280',
-                fontSize: 11,
-                marginTop: 4,
-              }}
-            >
-              {bus.capacity}% Full
-            </Text>
-
-            <Text
-              style={{
-                fontWeight: 'bold',
-                color: '#6B7280',
-                fontSize: 11,
-                marginTop: 4,
-              }}
-            >
-              {bus.speed.toFixed(0)} MPH
-            </Text>
-          </View>
-        </View>
-      </Callout>
+      <BusCallout route={route} bus={bus} />
     </Marker>
   );
 };
--- src/app/components/map/markers/StopMarker.tsx
+++ src/app/components/map/markers/StopMarker.tsx
@@ -1,16 +1,11 @@
 import { getLighterColor } from '@data/utils/utils';
 import React, { memo, useEffect } from 'react';
-import { ActivityIndicator, Platform, Text, View } from 'react-native';
-import { Callout, MapMarker, Marker } from 'react-native-maps';
+import { View } from 'react-native';
+import { MapMarker, Marker } from 'react-native-maps';
+import StopCallout from '../StopCallout';
 
-import { useStopAmenities, useStopEstimate } from '@data/queries/app';
 import useAppStore from '@data/state/app_state';
 import { Direction, Route, Stop } from '@data/types';
-import moment from 'moment';
-import { lightMode } from 'src/app/theme';
-import AmenityRow from '../../ui/AmenityRow';
-import BusIcon from '../../ui/BusIcon';
-import CalloutTimeBubble from '../../ui/CalloutTimeBubble';
 
 interface Props {
   stop: Stop;
@@ -48,16 +43,6 @@ const StopMarker: React.FC<Props> = ({
     }
   };
 
-  const scrollToStop = useAppStore((state) => state.scrollToStop);
-
-  const { data: estimates, isLoading } = useStopEstimate(
-    route,
-    direction,
-    stop,
-  );
-
-  const { data: amenities } = useStopAmenities(route, direction, stop);
-
   return (
     <Marker
       ref={markerRef}
@@ -81,98 +66,12 @@ const StopMarker: React.FC<Props> = ({
           elevation: active ? 700 : 400,
         }}
       />
-      <Callout
-        style={{
-          alignItems: 'center',
-          justifyContent: 'center',
-          width: 215,
-          height: Platform.OS === 'android' ? 60 + 18 : 60,
-          zIndex: 1000,
-          elevation: 1000,
-        }}
-        onPress={() => {
-          setSelectedDirection(direction);
-          scrollToStop(stop);
-        }}
-      >
-        <View
-          style={[
-            Platform.OS === 'android' && {
-              padding: 4,
-            },
-          ]}
-        >
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'center',
-              alignItems: 'center',
-              alignSelf: 'flex-start',
-            }}
-          >
-            <BusIcon
-              name={route.routeCode}
-              color={tintColor}
-              isCallout={true}
-              style={{ marginRight: 8 }}
-            />
-            <Text
-              style={{ flex: 1, fontWeight: 'bold', color: 'black' }}
-              numberOfLines={2}
-            >
-              {stop.name}
-            </Text>
-            <AmenityRow
-              amenities={amenities || []}
-              color={lightMode.subtitle}
-              size={18}
-            />
-          </View>
-
-          {isLoading ? (
-            <ActivityIndicator style={{ marginTop: 8 }} />
-          ) : estimates && estimates.length > 0 ? (
-            <View
-              style={{
-                flexDirection: 'row',
-                justifyContent: 'center',
-                alignItems: 'center',
-                alignSelf: 'flex-start',
-                marginTop: 8,
-              }}
-            >
-              <View style={{ flex: 1 }} />
-              {estimates!.map((estimate, index) => {
-                const date = estimate.estimatedTime ?? estimate.scheduledTime;
-                const relative = date.diff(moment(), 'minutes');
-                return (
-                  <CalloutTimeBubble
-                    key={`stop-callout-${index}`}
-                    time={relative <= 0 ? 'Now' : relative.toString() + ' min'}
-                    color={
-                      index === 0 ? tintColor + '50' : lightMode.nextStopBubble
-                    }
-                    textColor={index === 0 ? tintColor : lightMode.text}
-                    live={estimate.estimatedTime === null ? false : true}
-                  />
-                );
-              })}
-              <View style={{ flex: 1 }} />
-            </View>
-          ) : (
-            <Text
-              style={{
-                marginTop: 8,
-                alignSelf: 'center',
-                color: lightMode.subtitle,
-                fontSize: 12,
-              }}
-            >
-              No upcoming departures
-            </Text>
-          )}
-        </View>
-      </Callout>
+      <StopCallout
+        stop={stop}
+        tintColor={tintColor}
+        route={route}
+        direction={direction}
+      />
     </Marker>
   );
 };
