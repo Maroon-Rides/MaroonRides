7cc0b17e6ad6505cd64f6568c47236f659ad7021
bwees <brandonwees@gmail.com>
2025-09-28 17:04:35 -0500

    chore: formatting

--- src/data/queries/structure/aggie_spirit.ts
+++ src/data/queries/structure/aggie_spirit.ts
@@ -243,8 +243,8 @@ export const useASTimetableEstimate = (
           route?.directions.length === 1
             ? route.directions[0]
             : route?.directions.find((d) => {
-              return d.name === routeStop.directionName;
-            });
+                return d.name === routeStop.directionName;
+              });
 
         return {
           dataSource: DataSource.AGGIE_SPIRIT,
@@ -322,8 +322,8 @@ export const useASStopSchedule = (stop: Stop | null, date: moment.Moment) => {
           route?.directions.length === 1
             ? route.directions[0]
             : route?.directions.find((d) => {
-              return d.name === routeStop.directionName;
-            });
+                return d.name === routeStop.directionName;
+              });
 
         return {
           dataSource: DataSource.AGGIE_SPIRIT,
