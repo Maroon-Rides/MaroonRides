2c4390a705f70380fac78e24dd5ff766fc51849d
bwees <brandonwees@gmail.com>
2025-11-13 12:57:30 -0600

    chore: build steps

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/react-native';
 import { Stack } from 'expo-router';
 
 Sentry.init({
-  dsn: 'https://154c61925de343ee8345d0048e9316c2@bugsink.maroonrides.app/1',
+  dsn: 'https://99465597676d456dbfb42f46f1fcae17@bugsink.maroonrides.app/2',
 
   // Adds more context data to events (IP address, cookies, user, etc.)
   // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
