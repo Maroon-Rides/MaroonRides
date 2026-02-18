797512753eaca77e9b70eb9818896b26acdde9b3
bwees <brandonwees@gmail.com>
2026-02-18 12:01:16 -0600

    chore: map utils

--- pmtiles/cstat.sh
+++ pmtiles/cstat.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+
+BASEMAP="https://build.protomaps.com/20260112.pmtiles"
+
 pmtiles extract \
-  https://build.protomaps.com/20260205.pmtiles \
+  $BASEMAP \
   cstat_8_16.pmtiles \
   --bbox=-96.525584,30.510059,-96.200881,30.720020 \
   --minzoom=12 \
   --maxzoom=16 \
 
 pmtiles extract \
-  https://build.protomaps.com/20260205.pmtiles \
+  $BASEMAP \
   cstat_0_8.pmtiles \
   --bbox=-96.604156,30.321781,-96.135035,30.868760 \
   --minzoom=0 \
