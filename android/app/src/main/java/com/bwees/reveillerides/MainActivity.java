5ad6c85ae2dc7ab97cc62e83844fa50149ed5f75
Michael <109261155+michaeljwalla@users.noreply.github.com>
2026-07-09 14:50:21 -0500

    fix: allow android app to ignore certificates (#16)
    Needed for weird cert issues on Android

--- android/app/src/main/java/com/bwees/reveillerides/MainActivity.java
+++ android/app/src/main/java/com/bwees/reveillerides/MainActivity.java
@@ -1,5 +1,48 @@
 package com.bwees.reveillerides;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // DANGER: Forces the native Java layer to accept expired/invalid certs
+        bypassNativeSSLChecks();
+    }
+
+    private void bypassNativeSSLChecks() {
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+                }
+            };
+
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new SecureRandom());
+            
+            // Apply trust-all logic globally to native requests
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String arg0, SSLSession arg1) {
+                    return true;
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
