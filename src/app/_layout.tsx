975e6c99b39cac0cdbe007ffbd7acc7d58702a89
bwees <brandonwees@gmail.com>
2026-07-09 21:14:46 -0500

    chore: remove sentry

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,27 +1,11 @@
-import * as Sentry from '@sentry/react-native';
 import { Stack } from 'expo-router';
 
-Sentry.init({
-  dsn: 'https://99465597676d456dbfb42f46f1fcae17@bugsink.maroonrides.app/2',
-
-  // Adds more context data to events (IP address, cookies, user, etc.)
-  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
-  sendDefaultPii: true,
-
-  // Enable Logs
-  enableLogs: false,
-});
-
-function Layout() {
+export default function Layout() {
   return (
-    <Sentry.ErrorBoundary>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-        }}
-      />
-    </Sentry.ErrorBoundary>
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    />
   );
 }
-
-export default Sentry.wrap(Layout);
